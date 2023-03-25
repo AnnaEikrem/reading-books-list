af3222553a36ed48fbb7395d6a9a27f7626b50b6
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-25 15:50:58 +0100

    Added summary data

--- _app/js/modules/booksData.js
+++ _app/js/modules/booksData.js
@@ -4,112 +4,128 @@ export const booksData = [
 	  author: "J.R.R. Tolkien",
 	  year: 1954,
 	  pages: 1178,
-	  genre: "fantasy"
+	  genre: "fantasy",
+	  summary: "A young hobbit named Frodo Baggins inherits a powerful ring that threatens to fall into the hands of the dark lord Sauron. With the help of a fellowship of diverse allies, Frodo sets out on a perilous journey to destroy the ring and save Middle-earth from certain doom."
 	},
 	{
 	  title: "1984",
 	  author: "George Orwell",
 	  year: 1949,
 	  pages: 328,
-	  genre: "dystopian"
+	  genre: "dystopian",
+	  summary: "In a totalitarian society where individual thought and expression are strictly regulated, Winston Smith begins to rebel against the oppressive government and falls in love with a fellow dissenter. But the consequences of his actions are severe and irreversible."
 	},
 	{
 	  title: "The Great Gatsby",
 	  author: "F. Scott Fitzgerald",
 	  year: 1925,
 	  pages: 180,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "Jay Gatsby, a mysterious millionaire with a dark past, throws lavish parties at his luxurious estate in pursuit of his former love, Daisy Buchanan. But as Gatsby's obsession with Daisy deepens, their idyllic romance is threatened by jealousy, corruption, and tragedy."
 	},
 	{
 	  title: "To Kill a Mockingbird",
 	  author: "Harper Lee",
 	  year: 1960,
 	  pages: 281,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "In the racially divided town of Maycomb, Alabama, young Scout Finch learns important lessons about justice, compassion, and the human experience from her father, Atticus, a principled lawyer defending a black man falsely accused of a crime."
 	},
 	{
 	  title: "Brave New World",
 	  author: "Aldous Huxley",
 	  year: 1932,
 	  pages: 288,
-	  genre: "dystopian"
+	  genre: "dystopian",
+	  summary: "In a future society where emotions and individuality are suppressed through genetic engineering, drugs, and conditioning, Bernard Marx and his lover, Lenina Crowne, begin to question the dehumanizing effects of their conformist existence."
 	},
 	{
 	  title: "Pride and Prejudice",
 	  author: "Jane Austen",
 	  year: 1813,
 	  pages: 279,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "In Georgian-era England, the Bennet sisters navigate the intricacies of love, marriage, and societal expectations. When the proud and wealthy Mr. Darcy crosses paths with the lively and independent Elizabeth Bennet, their initial prejudices and misunderstandings give way to a mutual admiration and respect."
 	},
 	{
 	  title: "The Hitchhiker's Guide to the Galaxy",
 	  author: "Douglas Adams",
 	  year: 1979,
 	  pages: 193,
-	  genre: "science fiction"
+	  genre: "science fiction",
+	  summary: "Seconds before Earth is demolished to make way for a galactic freeway, Arthur Dent is rescued by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker's Guide to the Galaxy. Together, they embark on a series of absurd and humorous adventures across the universe."
 	},
 	{
 	  title: "The Alchemist",
 	  author: "Paulo Coelho",
 	  year: 1988,
 	  pages: 197,
-	  genre: "self-development"
+	  genre: "self-development",
+	  summary: "Santiago, a shepherd boy from Andalusia, sets out on a journey to fulfill his personal legend and find a treasure hidden in the Egyptian pyramids. Along the way, he encounters a series of mentors and obstacles that teach him valuable lessons about the nature of life and the universe."
 	},
 	{
 	  title: "The Hunger Games",
 	  author: "Suzanne Collins",
 	  year: 2008,
 	  pages: 374,
-	  genre: "dystopian"
+	  genre: "dystopian",
+	  summary: "In a dystopian future, the totalitarian government of Panem forces children from each of its 12 districts to compete in a televised battle to the death. When 16-year-old Katniss Everdeen volunteers to take her younger sister's place, she must use all her skills and wits to survive and challenge the corrupt system."
 	},
 	{
 	  title: "The Catcher in the Rye",
 	  author: "J.D. Salinger",
 	  year: 1951,
 	  pages: 277,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "Holden Caulfield, a teenage boy, narrates his experiences in New York City after being expelled from his prep school."
 	},
 	{
 	  title: "The Chronicles of Narnia",
 	  author: "C.S. Lewis",
 	  year: 1950,
 	  pages: 767,
-	  genre: "fantasy"
+	  genre: "fantasy",
+	  summary: "A group of children enter the magical world of Narnia and help Aslan, the lion, defeat the evil White Witch."
 	},
 	{
 	  title: "Harry Potter and the Philosopher's Stone",
 	  author: "J.K. Rowling",
 	  year: 1997,
 	  pages: 223,
-	  genre: "fantasy"
+	  genre: "fantasy",
+	  summary: "Harry Potter discovers he is a wizard and attends Hogwarts School of Witchcraft and Wizardry, where he battles the evil Lord Voldemort."
 	},
 	{
 	  title: "The Picture of Dorian Gray",
 	  author: "Oscar Wilde",
 	  year: 1890,
 	  pages: 254,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "Dorian Gray, a handsome and wealthy young man, sells his soul in exchange for eternal youth and beauty, but his portrait ages and reveals his sins."
 	},
 	{
 	  title: "The Road",
 	  author: "Cormac McCarthy",
 	  year: 2006,
 	  pages: 287,
-	  genre: "dystopian"
+	  genre: "dystopian",
+	  summary: "A father and his young son journey through a post-apocalyptic wasteland, facing starvation, danger, and despair."
 	},
 	{
 	  title: "Animal Farm",
 	  author: "George Orwell",
 	  year: 1945,
 	  pages: 112,
-	  genre: "dystopian"
+	  genre: "dystopian",
+	  summary: "After overthrowing their human owner, the farm animals of Manor Farm establish their own socialist utopia, with the pigs taking leadership roles. But as the pigs become corrupted by power and betray their fellow animals, the ideals of the revolution are subverted and distorted."
 	},
 	{
 	  title: "The Art of War",
 	  author: "Sun Tzu",
 	  year: -500,
 	  pages: 48,
-	  genre: "self-development"
+	  genre: "self-development",
+	  summary: "Sun Tzu's ancient Chinese treatise on military strategy and tactics, emphasizing the importance of deception, surprise, and flexibility."
 	},
 	{
 	  title: "The Stand",
@@ -117,28 +133,30 @@ export const booksData = [
 	  year: 1978,
 	  pages: 823,
 	  genre: 'dystopia',
+	  summary: "After a deadly pandemic wipes out most of the world's population, the survivors must rebuild society and confront supernatural forces."
 	},
 	{
 	  title: "The Picture of Dorian Gray",
 	  author: "Oscar Wilde",
 	  publicationDate: "1890",
 	  numberOfPages: 254,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "Dorian Gray, a wealthy and handsome young man, becomes corrupted by his own beauty and hedonism, leading to tragic consequences."
 	},
 	{
 	  title: "Fahrenheit 451",
 	  author: "Ray Bradbury",
 	  publicationDate: "1953",
 	  numberOfPages: 158,
-	  genre: "dystopian"
+	  genre: "dystopian",
+	  summary: "In a future society where books are banned and 'firemen' burn them, a fireman named Montag becomes disillusioned and joins the rebellion."
 	},
 	{
 	  title: "The Color Purple",
 	  author: "Alice Walker",
 	  publicationDate: "1982",
 	  numberOfPages: 295,
-	  genre: "classic"
+	  genre: "classic",
+	  summary: "Celie, a young African-American woman, overcomes abuse, oppression, and bigotry to find love, empowerment, and self-discovery."
 	}
 ]
-
-
