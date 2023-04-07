ba8e6264b9999f6b7d70108d7d47ed7bacfe846b
Anna Eikrem <eikrem.anna@gmail.com>
2023-04-07 21:36:06 +0200

    Created toggleNavigation for header

---
+++ _app/js/modules/toggleNavigation.js
@@ -0,0 +1,37 @@
+export default function toggleNavigation() {
+	let navigationVisible = false;
+
+	const navigationMenu = document.querySelector('.header__navigation');
+	const burgerMenuIcon = document.querySelector('.header__navigation--open');
+	const closeMenuIcon = document.querySelector('.header__navigation--close');
+
+	if (burgerMenuIcon !== null) {
+		burgerMenuIcon.addEventListener('click', handleBurgerMenuIconClick);
+	};
+
+	if (closeMenuIcon !== null) {
+		closeMenuIcon.addEventListener('click', handleCloseMenuIconClick);
+	}
+
+	function handleBurgerMenuIconClick() {
+		toggleNavigation();
+	}
+
+	function handleCloseMenuIconClick() {
+		toggleNavigation();
+	}
+
+	function toggleNavigation() {
+		navigationVisible = !navigationVisible;
+
+		if (!navigationVisible) {
+			navigationMenu.style.display = 'none';
+			burgerMenuIcon.style.display = 'inline';
+			closeMenuIcon.style.display = 'none';
+		} else {
+			navigationMenu.style.display = 'block';
+			burgerMenuIcon.style.display = 'none';
+			closeMenuIcon.style.display = 'inline';
+		}
+	}
+}
