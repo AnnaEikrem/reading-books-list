928bf1f6f6f50f0c70189aaaa9f17799514a10eb
Anna Eikrem <eikrem.anna@gmail.com>
2023-05-09 21:49:28 +0200

    Added alternative text to booksData

--- _app/js/modules/booksData.js
+++ _app/js/modules/booksData.js
@@ -8,6 +8,7 @@ export const booksData = [
 		genreName: 'Fantasy',
 		summary: "A young hobbit named Frodo Baggins inherits a powerful ring that threatens to fall into the hands of the dark lord Sauron. With the help of a fellowship of diverse allies, Frodo sets out on a perilous journey to destroy the ring and save Middle-earth from certain doom.",
 		coverUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg',
+		altText: 'Lord of the Rings book cover',
 		slug: 'the-lord-of-the-rings',
 	//   buyLink: 'https://www.amazon.com/Lord-Rings-Illustrated-J-R-R-Tolkien/dp/0358653037/ref=sr_1_6?crid=2WPFIN6F959NJ&keywords=the+lord+of+the+rings+book&qid=1679853655&sprefix=the+lord+of+the+rings+book%2Caps%2C177&sr=8-6'
 	},
@@ -20,6 +21,7 @@ export const booksData = [
 		genreName: "Dystopian",
 		summary: "In a totalitarian society where individual thought and expression are strictly regulated, Winston Smith begins to rebel against the oppressive government and falls in love with a fellow dissenter. But the consequences of his actions are severe and irreversible.",
 		coverUrl:'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91SZSW8qSsL.jpg',
+		altText: '1984 book cover',
 		slug: '1984'
 	},
 	{
@@ -31,6 +33,7 @@ export const booksData = [
 		genreName: "Classic",
 		summary: "Jay Gatsby, a mysterious millionaire with a dark past, throws lavish parties at his luxurious estate in pursuit of his former love, Daisy Buchanan. But as Gatsby's obsession with Daisy deepens, their idyllic romance is threatened by jealousy, corruption, and tragedy.",
 		coverUrl: 'https://kbimages1-a.akamaihd.net/bb4ae37a-8370-4c31-aea8-55a51ba734b7/1200/1200/False/the-great-gatsby-the-original-1925-edition-a-f-scott-fitzgerald-classic-novel.jpg',
+		altText: 'The Great Gatsby book cover',
 		slug: 'the-great-gatsby'
 	},
 	{
@@ -42,6 +45,7 @@ export const booksData = [
 		genreName: "Classic",
 		summary: "In the racially divided town of Maycomb, Alabama, young Scout Finch learns important lessons about justice, compassion, and the human experience from her father, Atticus, a principled lawyer defending a black man falsely accused of a crime.",
 		coverUrl: 'https://i.pinimg.com/originals/0b/bf/b5/0bbfb59b4d5592e2e7fac9930012ce6d.jpg',
+		altText: 'To Kill a Mockingbird book cover',
 		slug: 'to-kill-a-mockingbird'
 	},
 	{
@@ -53,6 +57,7 @@ export const booksData = [
 		genreName: "Dystopian",
 		summary: "In a future society where emotions and individuality are suppressed through genetic engineering, drugs, and conditioning, Bernard Marx and his lover, Lenina Crowne, begin to question the dehumanizing effects of their conformist existence.",
 		coverUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/62/BraveNewWorld_FirstEdition.jpg/220px-BraveNewWorld_FirstEdition.jpg',
+		altText: 'Brave New World book cover',
 		slug: 'brave-new-world'
 	},
 	{
@@ -64,6 +69,7 @@ export const booksData = [
 		genreName: "Classic",
 		summary: "In Georgian-era England, the Bennet sisters navigate the intricacies of love, marriage, and societal expectations. When the proud and wealthy Mr. Darcy crosses paths with the lively and independent Elizabeth Bennet, their initial prejudices and misunderstandings give way to a mutual admiration and respect.",
 		coverUrl: 'https://i.pinimg.com/originals/31/ff/8d/31ff8df674edf7c4071a4d2e63bb53b7.jpg',
+		altText: 'Pride and Prejudice book cover',
 		slug: 'pride-and-prejudice'
 	},
 	{
@@ -75,6 +81,7 @@ export const booksData = [
 		genreName: "Science fiction",
 		summary: "Seconds before Earth is demolished to make way for a galactic freeway, Arthur Dent is rescued by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker's Guide to the Galaxy. Together, they embark on a series of absurd and humorous adventures across the universe.",
 		coverUrl: 'https://covers.openlibrary.org/b/id/8594912-L.jpg',
+		altText: 'The Hitchhiker"s Guide to the Galaxy book cover',
 		slug: 'the-hitchhikers-guide-to-the-galaxy'
 	},
 	{
@@ -86,6 +93,7 @@ export const booksData = [
 		genreName: "Self-development",
 		summary: "Santiago, a shepherd boy from Andalusia, sets out on a journey to fulfill his personal legend and find a treasure hidden in the Egyptian pyramids. Along the way, he encounters a series of mentors and obstacles that teach him valuable lessons about the nature of life and the universe.",
 		coverUrl: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg',
+		altText: 'The Alchemist book cover',
 		slug: 'the-alchemist'
 	},
 	{
@@ -97,6 +105,7 @@ export const booksData = [
 		genreName: "Dystopian",
 		summary: "In a dystopian future, the totalitarian government of Panem forces children from each of its 12 districts to compete in a televised battle to the death. When 16-year-old Katniss Everdeen volunteers to take her younger sister's place, she must use all her skills and wits to survive and challenge the corrupt system.",
 		coverUrl: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41tqzW6kO7S.jpg',
+		altText: 'The Hunger Games book cover',
 		slug: 'the-hunger-games'
 	},
 	{
@@ -108,6 +117,7 @@ export const booksData = [
 		genreName: "Classic",
 		summary: "Holden Caulfield, a teenage boy, narrates his experiences in New York City after being expelled from his prep school.",
 		coverUrl: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/91HPG31dTwL.jpg',
+		altText: 'The Catcher in the Rye book cover',
 		slug: 'the-catcher-in-the-rye'
 	},
 	{
@@ -119,6 +129,7 @@ export const booksData = [
 		genreName: "Fantasy",
 		summary: "A group of children enter the magical world of Narnia and help Aslan, the lion, defeat the evil White Witch.",
 		coverUrl: 'https://storage.googleapis.com/lr-assets/kids/covers/_hires_imported/9780007100248.jpg',
+		altText: 'The Chronicles of Narnia book cover',
 		slug: 'the-chronicles-of-Narnia'
 	},
 	{
@@ -130,6 +141,7 @@ export const booksData = [
 		genreName: "Fantasy",
 		summary: "Harry Potter discovers he is a wizard and attends Hogwarts School of Witchcraft and Wizardry, where he battles the evil Lord Voldemort.",
 		coverUrl: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51mtZy7oJVL.jpg',
+		altText: 'Harry Potter and the Philsosophers"s Stone book cover',
 		slug: 'harry-potter-and-the-philosophers-stone'
 	},
 	{
@@ -141,6 +153,7 @@ export const booksData = [
 		genreName: "Classic",
 		summary: "Dorian Gray, a handsome and wealthy young man, sells his soul in exchange for eternal youth and beauty, but his portrait ages and reveals his sins.",
 		coverUrl: 'https://cdn.waterstones.com/bookjackets/large/9781/4351/9781435129757.jpg',
+		altText: 'The Picture of Dorian Gray book cover',
 		slug: 'the-picture-of-dorian-gray'
 	},
 	{
@@ -152,6 +165,7 @@ export const booksData = [
 		genreName: "Dystopian",
 		summary: "A father and his young son journey through a post-apocalyptic wasteland, facing starvation, danger, and despair.",
 		coverUrl: 'https://booklistqueen.com/wp-content/uploads/the-road-cormac-mccarthy.jpg',
+		altText: 'The Road book cover',
 		slug: 'the-road'
 	},
 	{
@@ -163,6 +177,7 @@ export const booksData = [
 		genreName: "Dystopian",
 		summary: "After overthrowing their human owner, the farm animals of Manor Farm establish their own socialist utopia, with the pigs taking leadership roles. But as the pigs become corrupted by power and betray their fellow animals, the ideals of the revolution are subverted and distorted.",
 		coverUrl: 'https://www.norli.no/media/catalog/product/cache/fe27bfb3093a1c90f11163d798c691b2/9/7/9780141036137_1.jpg?auto=webp&format=pjpg',
+		altText: 'Animal Farm book cover',
 		slug: 'animal-farm'
 	},
 	{
@@ -174,6 +189,7 @@ export const booksData = [
 		genreName: "Self-development",
 		summary: "Sun Tzu's ancient Chinese treatise on military strategy and tactics, emphasizing the importance of deception, surprise, and flexibility.",
 		coverUrl: 'https://www.basicbooks.com/wp-content/uploads/2022/01/9780465013272-1.jpg?fit=1694%2C2600',
+		altText: 'The Art of War book cover',
 		slug: 'the-art-of-war'
 	},
 	{
@@ -185,6 +201,7 @@ export const booksData = [
 		genreName: 'Dystopian',
 		summary: "After a deadly pandemic wipes out most of the world's population, the survivors must rebuild society and confront supernatural forces.",
 		coverUrl: 'https://1.bp.blogspot.com/-RNSnS4eD0Hc/XgP_s5rsbyI/AAAAAAAARRY/lwLgCuPJWrQDcrxTa1U72YMC5LYnwqmGwCLcBGAsYHQ/s1600/Stand%252C%2BThe%2B-%2BComplete%2BUncut%2B-%2BStephen%2BKing%2B-%2BSignet%2BBooks%2B-%2BMay%2B1991.jpg',
+		altText: 'The Stand book cover',
 		slug: 'the-stand'
 	},
 	{
@@ -196,6 +213,7 @@ export const booksData = [
 		genreName: "Dystopian",
 		summary: "In a future society where books are banned and 'firemen' burn them, a fireman named Montag becomes disillusioned and joins the rebellion.",
 		coverUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpVj0dfsboEyXal888VBhi3ZL7kU8Oyf15EToRP9i6FMxkCEj0eBQKSP4qPy7s7Vyn9WI&usqp=CAU',
+		altText: 'Fahreneit 451 book cover',
 		slug: 'fahrenheit-451'
 	},
 	{
@@ -207,6 +225,7 @@ export const booksData = [
 		genreName: "Classic",
 		summary: "Celie, a young African-American woman, overcomes abuse, oppression, and bigotry to find love, empowerment, and self-discovery.",
 		coverUrl: 'https://i.etsystatic.com/11749085/r/il/9a296d/1913226742/il_570xN.1913226742_pn9h.jpg',
+		altText: 'The Color Purple book cover',
 		slug: 'the-color-purple'
 	},
 	{
@@ -218,6 +237,7 @@ export const booksData = [
 		genreName: "History",
 		summary: "In this international bestseller, Yuval Noah Harari explores the history of Homo sapiens, from the emergence of our species in Africa to the present day. He examines the key events and developments that have shaped our history, and argues that the success of Homo sapiens can be attributed to our ability to create and believe in shared myths and stories. Harari's provocative and thought-provoking book has been praised for its engaging writing style and its ability to challenge readers' assumptions about human nature and history.",
 		coverUrl: "https://s1.adlibris.com/images/17322680/sapiens-a-brief-history-of-humankind.jpg",
+		altText: 'Sapiens: A Brief History of Humankind book cover',
 		slug: 'sapiens-a-brief-history-of-humankind'
   },
 ]
